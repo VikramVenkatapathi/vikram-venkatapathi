78e2c708b9bf7aa80880d62b86034ad8b105262e
vikramv <vk485591@dal.ca>
2024-06-24 00:26:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import './assets/vendor/boxicons/css/boxicons.min.css'
+import './assets/vendor/glightbox/css/glightbox.min.css'
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Main from './Components/Main';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background: "rgba(46, 45, 43, 1)"}}>
+      <Header />
+      <Hero />
+      <Main />
     </div>
   );
 }
